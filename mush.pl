7b2b7b7392c356100b239c6093acdf94f767bb09
Dave Lambley <dave@lambley.me.uk>
2016-06-07 22:09:08 +0100

    Mush one.

--- mush.pl
+++ mush.pl
@@ -19,6 +19,7 @@ my @last;
 my $fps = 30;
 
 my $frame = 0;
+my $start_angle = 135;
 
 my $start_time;
 
@@ -46,9 +47,23 @@ DONE: foreach my $track (@{ $gpx->tracks }) {
 
     
                 while ($frame <= ($last[2]->{time}-$start_time)*$fps) {
-                    my $speed = sprintf("%02f", $interpolate->y($frame / 30.0));
-                    print "$speed\n";
-                    system("convert", "-size", "640x480", "xc:none", "-stroke", "white", "-pointsize", 40, "-gravity", "SouthEast", "-draw", "text 10,10 '$speed'", "frame$frame.png")==0 or die;
+                    my $kph = $interpolate->y($frame / 30.0)*3.6;
+#                    my $kph = $frame % 28;
+                    my $speed = sprintf("%.1fkph", $kph);
+                    my $bright = ($kph/28.8)*20+50;
+                    my $angle = $start_angle + (1/40+$kph / 70)*360;
+
+                    my $inner_thick = 5+($kph/28.8)*3;
+                    my $outer_thick = $inner_thick+2;
+
+                    print "$angle\n";
+
+                    system("convert", "-size", "640x480", "xc:none", "-fill", "none",
+                        "-stroke", "white", "-strokewidth", "2", "-fill", "blue", "-pointsize", 40, "-gravity", "SouthEast", "-draw", "text 10,10 '$speed'",
+                        "-fill", "none", "-stroke", "white", "-strokewidth", $outer_thick, "-draw", "ellipse 590,400 100,100 $start_angle,$angle",
+                        "-fill", "none", "-stroke", "hsl(0%, 80%, $bright%", "-strokewidth", $inner_thick, "-draw", "ellipse 590,400 100,100 $start_angle,$angle",
+                        "frame$frame.png"
+                    )==0 or die;
                     $frame++;
                 }
             }
