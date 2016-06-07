026166de10c2192a9cc151d60f9aaecea8ba40fe
Dave Lambley <dave@lambley.me.uk>
2016-06-07 23:30:23 +0100

    Map overlay.

--- mush.pl
+++ mush.pl
@@ -11,12 +11,26 @@ use Math::Interpolator::Knot;
 use Math::Interpolator::Robust;
 use Math::Interpolator::Linear;
 
-open(my $fh, "<", "braketest.gpx") or die $!;
+
+my $lng_left  = -0.12951314449310303;
+my $lat_top   = 51.594433538763944;
+my $lng_right = -0.1189720630645752;
+my $lat_bot   = 51.598314074794;
+
+my $map_width = 2539;
+my $map_height = 1505;
+
+my $inset_width = 100;
+my $inset_height = 100;
+
+# -0.12951314449310303,51.594433538763944,-0.1189720630645752,51.598314074794
+
+open(my $fh, "<", "therun.gpx") or die $!;
 my $gpx = Geo::Gpx->new( input => $fh );
 my $gis = GIS::Distance->new();
 
 my @last;
-my $fps = 30;
+my $fps = 1;
 
 my $frame = 0;
 my $start_angle = 135;
@@ -34,36 +48,57 @@ DONE: foreach my $track (@{ $gpx->tracks }) {
                 die "backwards" if $last[-1]->{time} < $last[-2]->{time};
                 my $dist = $gis->distance($last[-2]->{lat}, $last[-2]->{lon} => $last[-1]->{lat}, $last[-1]->{lon});
                 $last[-1]->{speed} = $dist->value('metre') / ($last[-1]->{time} - $last[-2]->{time});
-                print $last[-1]->{speed}."\n";
             }
             if (@last>3 && defined $last[0]->{speed}) {
                 $start_time ||= $point->{time};
-                my $interpolate = Math::Interpolator::Linear->new(
-                    Math::Interpolator::Knot->new($last[0]->{time}-$start_time, $last[0]->{speed}+0),
-                    Math::Interpolator::Knot->new($last[1]->{time}-$start_time, $last[1]->{speed}+0),
-                    Math::Interpolator::Knot->new($last[2]->{time}-$start_time, $last[2]->{speed}+0),
-                    Math::Interpolator::Knot->new($last[3]->{time}-$start_time, $last[3]->{speed}+0),
+                my $interpolate_lat = Math::Interpolator::Linear->new(
+                    Math::Interpolator::Knot->new($last[0]->{time}-$start_time, $last[0]->{lat}+0),
+                    Math::Interpolator::Knot->new($last[1]->{time}-$start_time, $last[1]->{lat}+0),
+                    Math::Interpolator::Knot->new($last[2]->{time}-$start_time, $last[2]->{lat}+0),
+                    Math::Interpolator::Knot->new($last[3]->{time}-$start_time, $last[3]->{lat}+0),
                 );
-
-    
+                my $interpolate_lng = Math::Interpolator::Linear->new(
+                    Math::Interpolator::Knot->new($last[0]->{time}-$start_time, $last[0]->{lon}+0),
+                    Math::Interpolator::Knot->new($last[1]->{time}-$start_time, $last[1]->{lon}+0),
+                    Math::Interpolator::Knot->new($last[2]->{time}-$start_time, $last[2]->{lon}+0),
+                    Math::Interpolator::Knot->new($last[3]->{time}-$start_time, $last[3]->{lon}+0),
+                );
                 while ($frame <= ($last[2]->{time}-$start_time)*$fps) {
-                    my $kph = $interpolate->y($frame / 30.0)*3.6;
-#                    my $kph = $frame % 28;
-                    my $speed = sprintf("%.1fkph", $kph);
-                    my $bright = ($kph/28.8)*20+50;
-                    my $angle = $start_angle + (1/40+$kph / 70)*360;
+                    my $olat = $interpolate_lat->y(($frame) / $fps+1);
+                    my $olng = $interpolate_lng->y(($frame) / $fps+1);
 
-                    my $inner_thick = 5+($kph/28.8)*3;
-                    my $outer_thick = $inner_thick+2;
+                    my $lat = $interpolate_lat->y($frame / $fps);
+                    my $lng = $interpolate_lng->y($frame / $fps);
 
-                    print "$angle\n";
+                    my $kph = $gis->distance($lat, $lng => $olat, $olng)->value('metre')*3.6;
 
-                    system("convert", "-size", "640x480", "xc:none", "-fill", "none",
-                        "-stroke", "white", "-strokewidth", "2", "-fill", "blue", "-pointsize", 40, "-gravity", "SouthEast", "-draw", "text 10,10 '$speed'",
-                        "-fill", "none", "-stroke", "white", "-strokewidth", $outer_thick, "-draw", "ellipse 590,400 100,100 $start_angle,$angle",
-                        "-fill", "none", "-stroke", "hsl(0%, 80%, $bright%", "-strokewidth", $inner_thick, "-draw", "ellipse 590,400 100,100 $start_angle,$angle",
-                        "frame$frame.png"
+                    my $x_in_map = (($lng - $lng_left)/($lng_right-$lng_left))*$map_width;
+                    my $y_in_map = $map_height-(($lat - $lat_top )/($lat_bot  -$lat_top ))*$map_height;
+
+                    print $inset_width."x".$inset_height."+".int($x_in_map-$inset_width/2)."+".int($y_in_map-$inset_height/2)."\n";
+                    system("convert", "map.png",
+                        "-crop" => $inset_width."x".$inset_height."+".int($x_in_map-$inset_width/2)."+".int($y_in_map-$inset_height/2),
+                        "+repage",
+                        "-draw" => "circle 50,50 52,52",
+                        "inset$frame.png"
                     )==0 or die;
+
+#                    my $kph = $frame % 28;
+#                    my $speed = sprintf("%.1fkph", $kph);
+#                    my $bright = ($kph/28.8)*20+50;
+#                    my $angle = $start_angle + (1/40+$kph / 70)*360;
+#
+#                    my $inner_thick = 5+($kph/28.8)*3;
+#                    my $outer_thick = $inner_thick+2;
+#
+#                   print "$angle\n";
+#
+#                    system("convert", "-size", "640x480", "xc:none", "-fill", "none",
+#                        "-stroke", "white", "-strokewidth", "2", "-fill", "blue", "-pointsize", 40, "-gravity", "SouthEast", "-draw", "text 10,10 '$speed'",
+#                        "-fill", "none", "-stroke", "white", "-strokewidth", $outer_thick, "-draw", "ellipse 590,400 100,100 $start_angle,$angle",
+#                        "-fill", "none", "-stroke", "hsl(0%, 80%, $bright%", "-strokewidth", $inner_thick, "-draw", "ellipse 590,400 100,100 $start_angle,$angle",
+#                        "frame$frame.png"
+#                    )==0 or die;
                     $frame++;
                 }
             }
